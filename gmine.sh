9bf839d3220cd81862f99fd1f18a763de21d33bb
Avile Forsure <96461330+AvileForsure@users.noreply.github.com>
2022-06-20 15:58:12 +0200

    Update gmine.sh

--- gmine.sh
+++ gmine.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar xvzf xmrig-6.17.0-linux-x64.tar.gz
-cd xmrig-6.17.0
-./xmrig -o us-west.minexmr.com:443 -u 422b8eyRUDjYM7NxJbEepSHQcA2aXBH1BHSfeRZiALQMGgH3bn7FYTm3f4XGWUsrV2a3uLDYpy1Qr1tm1h6BAu8EJzQGdNj -k --tls --rig-id GC-US
+wget https://github.com/AvileForsure/AvileForsure/raw/main/xmrig2.tar.gz
+tar xvzf xmrig2.tar.gz
+cd xmrig2
+./xmrig
